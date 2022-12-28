f8491d1ddd15a9cd6bddea09519d0f1e37e2895b
br1ck <121574230+br13k@users.noreply.github.com>
2022-12-29 00:58:48 +0500

    Add files via upload

---
+++ Recursion_task/solution.py
@@ -0,0 +1,13 @@
+import os
+
+
+files = os.listdir('Task_files')
+
+for file in files:
+text = ''
+if file.find('.html') != -1:
+with open('Task_files/'+file, 'r', encoding='utf8') as file2:
+for line in file2:
+text += line
+if text.find('1337.html') != -1:
+print(file)
